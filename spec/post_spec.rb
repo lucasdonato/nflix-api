e7aa64bf6be29ac7958b8c1b4657c01c9dd31722
=lucasdonato <=lucaspolimig96@gmail.com>
2019-06-24 13:23:30 -0300

    criando projeto nflix-api

---
+++ spec/post_spec.rb
@@ -0,0 +1,19 @@
+
+
+describe "post" do
+  context "when new user" do
+    before do
+      @new_user = build(:user).to_hash
+
+      @result = HTTParty.post(
+        "http://localhost:3001/user",
+        body: @new_user.to_json,
+        headers: {
+          "Content-Type" => "application/json",
+        },
+      )
+    end
+
+    it { expect(@result.response.code).to eql "200" }
+  end
+end
